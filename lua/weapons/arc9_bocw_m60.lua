008c06e416128fbb53db4cf9993e53895632cf9b
multinett <72457258+multinettt@users.noreply.github.com>
2025-12-06 17:53:50 +0300

    Integrate Options

--- lua/weapons/arc9_bocw_m60.lua
+++ lua/weapons/arc9_bocw_m60.lua
@@ -247,7 +247,15 @@ SWEP.ShootWhileSprint = false
 SWEP.Speed = 1
 
 SWEP.SpeedMult = 0.95
-SWEP.SpeedMultSprint = 0.6374269005847955
+
+local SprintMultCvar = GetConVar("arc9_bocw_sprintmultiplier")
+
+if SprintMultCvar:GetBool() then
+    SWEP.SpeedMultSprint = 0.6374269005847955 -- SPRINT SPEED RELATIVE TO MOVE SPEED
+else
+    SWEP.SpeedMultSprint = 1 -- this may possibly change later
+end
+
 SWEP.SpeedMultSights = 0.2680311890838207
 SWEP.SpeedMultShooting = 0.6101364522417153
 SWEP.SpeedMultMelee = 1
@@ -754,12 +762,22 @@ SWEP.Attachments = {
         RequireElements = {"bocw_m60_magazine_mixpro"}
     },
 }
+--=========================================================
+-- CUSTOM SLOT INSERTION
+local CustomSlotCvar = GetConVar("arc9_bocw_customslot")
+local CustomSlotTable = {
+    PrintName = "Custom Slot",
+    Bone = "tag_weapon",
+    Pos = Vector(0, 0, 0),
+    Ang = Angle(0, 0, 0),
+    Icon_Offset = Vector(-12, 0, 3),
+    Category = {"bocw_custom_slot", "bocw_custom_slot_m60"},
+}
 
-SWEP.Hook_ModifyBodygroups = function(self, data)
-
-    local vm = data.model
-    local attached = data.elements
+if CustomSlotCvar:GetBool() then
+    table.insert(SWEP.Attachments, CustomSlotTable)
 end
+--=========================================================
 
 SWEP.Hook_TranslateAnimation = function(swep, anim)
     
--- lua/weapons/arc9_bocw_stoner63.lua
+++ lua/weapons/arc9_bocw_stoner63.lua
@@ -250,7 +250,15 @@ SWEP.ShootWhileSprint = false
 SWEP.Speed = 1
 
 SWEP.SpeedMult = 0.95
-SWEP.SpeedMultSprint = 0.6374269005847955
+
+local SprintMultCvar = GetConVar("arc9_bocw_sprintmultiplier")
+
+if SprintMultCvar:GetBool() then
+    SWEP.SpeedMultSprint = 0.6374269005847955 -- SPRINT SPEED RELATIVE TO MOVE SPEED
+else
+    SWEP.SpeedMultSprint = 1 -- this may possibly change later
+end
+
 SWEP.SpeedMultSights = 0.2680311890838207
 SWEP.SpeedMultShooting = 0.6101364522417153
 SWEP.SpeedMultMelee = 1
@@ -742,12 +750,22 @@ SWEP.Attachments = {
         RequireElements = {"stoner63_mag_mix"}
     },
 }
+--=========================================================
+-- CUSTOM SLOT INSERTION
+local CustomSlotCvar = GetConVar("arc9_bocw_customslot")
+local CustomSlotTable = {
+    PrintName = "Custom Slot",
+    Bone = "tag_weapon",
+    Pos = Vector(0, 0, 0),
+    Ang = Angle(0, 0, 0),
+    Icon_Offset = Vector(-4, 0, 3),
+    Category = {"bocw_custom_slot", "bocw_custom_slot_stoner63"},
+}
 
-SWEP.Hook_ModifyBodygroups = function(self, data)
-
-    local vm = data.model
-    local attached = data.elements
+if CustomSlotCvar:GetBool() then
+    table.insert(SWEP.Attachments, CustomSlotTable)
 end
+--=========================================================
 
 SWEP.Hook_TranslateAnimation = function(swep, anim)
     
